becfc2747d88c3bdd83b96254a5bde0ae1836783
Max Wofford <max@hackedu.us>
2015-02-05 05:45:14 +0000

    support for phantomjs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Creative market downloader
-
+This was built to learn selenium. It someone wants to make this better by building it with nokogiri, be my guest.
 ### Install && Run
 
 ```
@@ -10,4 +10,10 @@ CM_PASSWORD = 'every_time_you_commit_a_key_and_orphan_puppy_gets_kicked_into_the
 ```
 ```
 bundle install && ruby app.rb
+```
+
+### Options
+```bash
+# runs in phantom by default
+--firefox # firefox
 ```
--- app.rb
+++ app.rb
@@ -1,15 +1,28 @@
 require 'selenium-webdriver'
 require_relative 'config'
 
+def is_flag_on?(flag)
+  ARGV.include?(flag)
+end
+
 def sign_in(driver)
+  puts 'signing in'
   driver.find_element(:id, 'input-username').send_keys(CM_USERNAME)
   driver.find_element(:id, 'input-password').send_keys(CM_PASSWORD)
   driver.find_element(:id, 'input-password').submit
+  puts 'signed in'
 end
 
-driver = Selenium::WebDriver.for :firefox
+if is_flag_on?("--firefox")
+  driver = Selenium::WebDriver.for :firefox
+else
+  driver = Selenium::WebDriver.for :phantomjs
+  driver.manage.window.resize_to(1000, 1000)
+end
+
 driver.navigate.to 'https://creativemarket.com/sign-in?r=%2Ffree-goods'
 sign_in(driver)
 elements = driver.find_elements(:class, 'btn-download')
+puts 'clicking the things'
 elements.each { |element| element.click}
 driver.quit
